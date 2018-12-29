09101afbaff62b9a741b59df218c3765814852a4
Jonathan Delgado <jonathandelgado0327@gmail.com>
2018-12-28 21:34:52 -0500

    Updated to include a remaining_line function that reads the remainder of a line, good for cleaning and moving onto next line

--- file.c
+++ file.c
@@ -1,6 +1,113 @@
+/***********************Include***********************/
 #include <stdlib.h>
 #include <ctype.h>
+
 #include "file.h"
+
+int next(FILE *fp, char *line) {
+    char current;
+    int index = 0;
+
+    //Ends at end of file, space, and newline to separate strings
+    while((current = getc(fp)) != EOF && !isspace(current))
+        line[index++] = current;
+
+    //If we're at the end of the file, and there was nothing read before
+    //the index will be 0, so we will be returning the flag for ending
+    if(current == EOF && index == 0)
+        return 0;
+    //If the index was never incremented, but it's also not the end of the file
+    //we must've read whitespace, call ourselves back to read again
+    else if(index == 0)
+        return next(fp, line);
+
+    //Ends the string
+    line[index++] = '\0';
+
+    //If the string only consists of the terminating character or less
+    //then you basically read nothing
+    return index;
+}
+
+/***********************Getters***********************/
+bool is_next_int(FILE *fp) {
+    char line[MAX_LINE_LENGTH];
+    int len;
+    //Immediately returns false if at EOF
+    if( (len = next(fp, line)) == 0)
+        return false;
+
+    //Rewind from before the number
+    fseek(fp, -len, SEEK_CUR);
+
+    //Initializes the index for the loop early to alter the starting point
+    //Takes the sign into consideration
+    int i = 0;
+    if(line[i] == '-') {
+        //Checks edge case where the string is just "-"
+        if(len == 2)
+            return false;
+
+        i++;
+    }
+    //Go through the string and check if it's a number
+    for(; i < (len - 1); i++) {
+        //If there is a single entry that is not numeric, the string overall isn't a number
+        //i.e. 74G823 is not a number
+        if(!isdigit(line[i]))
+            return false;
+    
+    }
+
+    return true;
+}
+
+bool is_next_double(FILE *fp) {
+    char line[MAX_LINE_LENGTH];
+    int len;
+
+    if( (len = next(fp, line)) == 0 )
+        return false;
+
+    //Rewind
+    fseek(fp, -len, SEEK_CUR);
+    //Parse string to check if it's a number
+    //There should only be one decimal
+    int decimal_count = 0;
+
+    //Notice that this works similarly to is_next_int
+    //and returns positively even if the next number is technically an int
+    //which just means that if we expect a float, and get an int
+    //we'll end up promoting it just as we would want
+    int i = 0;
+    if(line[i] == '-') {
+        //Checks edge case where the string is just "-"
+        if(len == 2)
+            return false;
+
+        i++;
+    }
+
+    for(; i < (len - 1); i++) {
+        if(line[i] == '.') {
+            //If there's two decimal points, it's not a float
+            //or if the decimal is at the end of the number (i.e. 352. )
+            if(++decimal_count > 1 || i == (len - 1))
+                return false;
+            
+            //Continue the loop, since we don't want to check if it's a digit,
+            //it clearly isn't, but it's still valid syntax
+            continue;
+        }
+        //You're not a decimal point, and you're not a number
+        //so you're not a valid float
+        if(!isdigit(line[i]))
+            return false;
+        
+    }
+
+    return true;
+}
 
 int next_int(FILE *fp, int *ip) {
     if(!is_next_int(fp))
@@ -41,98 +148,4 @@ int next_doubles(FILE *fp, int times, double *dp) {
     return 1;
 }
 
-bool is_next_int(FILE *fp) {
-    char line[MAX_LINE_LENGTH];
-    int len;
- 
-    if((len = next(fp, line)) == -1)
-        return false;
-
-    //Rewind from before the number
-    fseek(fp, -len, SEEK_CUR);
-
-    //Initializes the index for the loop early to alter the starting point
-    //Takes the sign into consideration
-    int i = 0;
-    if(line[i] == '-')
-        i++;
-    //Go through the string and check if it's a number
-    for(; i < (len - 1); i++) {
-        //If there is a single entry that is not numeric, the string overall isn't a number
-        //i.e. 74G823 is not a number
-        if(!isdigit(line[i]))
-            return false;
-    
-    }
-
-    return true;
-}
-
-bool is_next_double(FILE *fp) {
-    char line[MAX_LINE_LENGTH];
-    int len;
-
-    if( (len = next(fp, line)) == -1 )
-        return false;
-
-    //Rewind
-    fseek(fp, -len, SEEK_CUR);
-
-    //Parse string to check if it's a number
-    //There should only be one decimal
-    int decimal_count = 0;
-
-    //Notice that this works similarly to is_next_int
-    //and returns positively even if the next number is technically an int
-    //which just means that if we expect a float, and get an int
-    //we'll end up promoting it just as we would want
-
-    int i = 0;
-    if(line[i] == '-')
-        i++;
-
-    for(; i < (len - 1); i++) {
-
-        if(line[i] == '.') {
-            //If there's two decimal points, it's not a float
-            if(++decimal_count > 1)
-                return false;
-            
-            //Continue the loop, since we don't want to check if it's a digit,
-            //it clearly isn't, but it's still valid syntax
-            continue;
-        }
-        //You're not a decimal point, and you're not a number
-        //so you're not a valid float
-        if(!isdigit(line[i]))
-            return false;
-        
-    }
-
-    return true;
-}
-
-int next(FILE *fp, char *line) {
-    char current;
-    int index = 0;
-
-    //Ends at end of file, space, and newline to separate strings
-    while((current = getc(fp)) != EOF && !isspace(current))
-        line[index++] = current;
-
-    //If we're at the end of the file, and there was nothing read before
-    //the index will be 0, so we will be returning the flag for ending
-    if(current == EOF && index == 0)
-        return 0;
-    //If the index was never incremented, but it's also not the end of the file
-    //we must've read whitespace, call ourselves back to read again
-    else if(index == 0)
-        return next(fp, line);
-
-    //Ends the string
-    line[index++] = '\0';
-
-    //If the string only consists of the terminating character or less
-    //then you basically read nothing
-    return index;
-}
+int remaining_line(FILE *fp, char *line) { return fgets(line, MAX_LINE_LENGTH, fp) != NULL; }
--- file.h
+++ file.h
@@ -4,7 +4,7 @@
 /******************************Define******************************/
 #define MAX_LINE_LENGTH 1000
 
-/******************************Prototypes******************************/
+/******************************PROTOTYPES******************************/
 //Gets the next string (also number as a string etc)
 //delimited by whitespace, newline, etc
 //and returns the length of the string
@@ -21,3 +21,8 @@ bool is_next_double(FILE *);
 int next_int(FILE *, int *);
 int next_double(FILE *, double *);
 int next_doubles(FILE *, int times, double *);
+//Reads the remaining line of the file
+//returns 1 if the pointer to the remainder of the file is store properly
+//retunrs 0 if there is no remaining text or other failure
+int remaining_line(FILE *, char *line);
+/***********************END PROTOTYPES***********************/
