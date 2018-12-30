3546b171b916183e09f681ffdaf4791b9ccc2d2d
Jonathan Delgado <jonathandelgado0327@gmail.com>
2018-12-29 19:31:41 -0500

    Performed more tests.

--- main.c
+++ main.c
@@ -1,20 +1,32 @@
+#include <stdlib.h>
 #include "file.h"
 
-//Do checks on next for EOF
 
 int main() {
     printf("\n");
     FILE *fp = fopen("input.txt", "r");
+    char *line = malloc(MAX_LINE_LENGTH);
+    int len;
 
-    char line[MAX_LINE_LENGTH];
-    int return_value;
 
-    while( (return_value = next(fp, line)) != 0) {
-    	printf("Next return value: %d\n", return_value);
-    	printf("String read: %s\n", line);
-	}
+    // while( fscanf(fp, "%s", line) != 0) {
 
-	printf("Next return value: %d\n", return_value);
+    //     printf("String read: %s\n", line);
+
+    //     if(getc(fp) == EOF) {
+    //         printf("End of file reached.\n");
+    //         break;
+    //     } else
+    //         fseek(fp, -1, SEEK_CUR);
+    // }  
+
+    // while( next(fp, line) != 0 ) {
+    //     printf("String read: %s\n", line);
+    // }
+
+    while(is_next_double(fp)) {
+        printf("Number read: %f\n", next_double(fp));
+    }
 
     fclose(fp);
     printf("\n");
